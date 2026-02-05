dfb943f6712f8a435569940232fcdef92e372dc1
Moony.dll <harrby14@icloud.com>
2026-02-06 00:20:21 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: '/',
+  plugins: [
+    react(),
+    ...
+  ],
+  ...
+})
+
   plugins: [
     react(),
     runtimeErrorOverlay(),
